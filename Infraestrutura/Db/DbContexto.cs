595147223a172bf60ef63a7e39d281106432136d
Guilherme Rondon <worktradestudygrra@gmail.com>
2024-08-29 13:39:40 -0300

    Iniciando projeto

---
+++ Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,10 @@
+namespace MinimmalAPI.Infraestrtutra.DB;
+
+puiblic class var {
+    
+} = new DbContextOptionsBuilder<ContextNameContext>();
+{
+    
+}.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Initial Catalog=DBName;Integrated Security=True");
+// var db = new ContextNameContext({
+}.Options)
---
+++ Program.cs
@@ -0,0 +1,51 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapPost("/Login", (MinimalAPI.DTOs.LoginDTO loginDTO) => {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com Sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
